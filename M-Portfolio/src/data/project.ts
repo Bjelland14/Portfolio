1f97121b46a3c8a02c95da839b0c2f75392e9fa9
Bjelland14 <lloydbjelland@gmail.com>
2026-02-26 18:29:35 +0100

    Update

---
+++ M-Portfolio/src/data/project.ts
@@ -0,0 +1,31 @@
+export type Project = {
+    title: string;
+    description: string;
+    image: string;
+    github: string;
+    live: string;
+};
+
+export const projects: Project[] = [
+  {
+    title: "RainyDays",
+    description: "An e-commerce website for a fictional clothing brand.",
+    image: "/M-Portfolio/public/assets/thumbnails/rainydays.png",
+    github: "https://github.com/Bjelland14/HTML-og-CSS-CA",
+    live: "https://bjelland14.github.io/HTML-og-CSS-CA/"
+  },
+  {
+    title: "RainyDays JavaScript",
+    description: "RainyDays project with focus on JavaScript functionality.",
+    image: "/M-Portfolio/public/assets/thumbnails/rainydays-js.png",
+    github: "https://github.com/Bjelland14/RainyDaysJS",
+    live: "https://bjelland14.github.io/RainyDaysJS/"
+  },
+  {
+    title: "Semester Project 1",
+    description: "Responsive and accessible museum website built with HTML & CSS.",
+    image: "/M-Portfolio/public/assets/thumbnails/SP-1.png",
+    github: "https://github.com/Bjelland14/Semester-Project-1",
+    live: "https://bjelland14.github.io/Semester-Project-1/"
+  }
+];
